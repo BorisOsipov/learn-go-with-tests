6e81cf4a4f8edc0ac8326fc2b09ab6407346448d
Boris Osipov <osipov.boris@gmail.com>
2019-05-03 11:34:21 +0300

    iteration

---
+++ iteration/repeat_test.go
@@ -0,0 +1,28 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("Expected '%s' but got '%s'", expected, repeated)
+	}
+
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("s", 10)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("w", 5)
+	fmt.Println(repeated)
+	// Output "wwwww"
+}
