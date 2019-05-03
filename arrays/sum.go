d8fa88c5ecfb39bbfaa93222e2469333ec784718
Boris Osipov <osipov.boris@gmail.com>
2019-05-03 13:26:06 +0300

    Array and slices

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,33 @@
 package main
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbresToSum ...[]int) []int {
+	lengthOfNumbers := len(numbresToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbresToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
+}
+
+func SumAllTails(numbresToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbresToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
 }
